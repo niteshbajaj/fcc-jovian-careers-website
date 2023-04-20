d5a17e839a5a23c5e14a5e2c4c5b0f25de1d3364
niteshbajaj <niteshsbajaj@gmail.com>
2023-04-20 13:19:30 +0000

    changes

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Anaylst',
+    'location': 'Mumbai',
+    'salary': '1 crores'
+  },
+  {
+    'id': 2,
+    'title': 'Data Anaylst',
+    'location': 'Mumbai',
+    'salary': '1 crores'
+  },
+  {
+    'id': 3,
+    'title': 'Data Anaylst',
+    'location': 'Mumbai',
+    'salary': '1 crores'
+  },
+  {
+    'id': 4,
+    'title': 'Data Anaylst',
+    'location': 'Powai',
+    'salary': '1 crores'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="NSB")
 
 
 if __name__ == '__main__':
